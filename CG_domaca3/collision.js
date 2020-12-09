dfdf9b3f7c95ae7ccf4b4fdc149b351b92543f9c
hugoiwnl <hugoserbia@gmail.com>
2020-12-09 19:07:17 +0100

    reseno quadtree, collision pri kraju

--- CG_domaca3/collision.js
+++ CG_domaca3/collision.js
@@ -5,6 +5,7 @@ class Ball{
         this.radius=radius;
         this.speedX=Math.random()*(3+5)-3;
         this.speedY=Math.random()*(3+5)-3;
+        this.collision=false;
         if(Math.floor(Math.random()*2) == 0){
             this.speedX*=-1;
         }
@@ -32,7 +33,40 @@ class Ball{
         //context.fillStyle="green";
         //context.fillRect(this.x-this.radius/2,this.y-this.radius/2,this.radius,this.radius);
         context.beginPath();
+        if(this.collision){
+            context.strokeStyle = "#FF0000";
+        }else{
+            context.strokeStyle = "#000000";
+        }
         context.arc(this.x,this.y,this.radius,0,2*Math.PI);
+        context.stroke();
+        context.closePath();
+    }
+
+    intersect(ball){
+        let prv=(ball.x-this.x);
+        let drug=(ball.y-this.y);
+        let d=Math.sqrt(Math.pow(prv,2)+Math.pow(drug,2));
+        return d<=this.r*2;
+    }
+}
+class Circle{
+    constructor(x,y,r){
+        this.x=x;
+        this.y=y;
+        this.r=r;
+    }
+    //nije dobar contains
+    contains(ball){
+        let prv=(ball.x-this.x);
+        let drug=(ball.y-this.y);
+        let d=Math.sqrt(Math.pow(prv,2)+Math.pow(drug,2));
+        return this.r>d+ball.radius;
+    }
+    draw(){
+        context.beginPath();
+        context.strokeStyle="#FF0000";
+        context.arc(this.x,this.y,this.r,0,2*Math.PI);
         context.stroke();
         context.closePath();
     }
@@ -51,6 +85,10 @@ class Rectangle{
     // da li su 2 rect-a u collisionu
     intersect(rect){
         return !(rect.y>this.y+this.height || rect.y+rect.height<this.y || rect.x>this.x+this.width || rect.x+rect.width<this.x);
+    }
+
+    intersectCircle(circle){
+        return !(circle.x-circle.r>this.x+this.width || circle.x+circle.r<this.x || circle.y-circle.r>this.y+this.h || circle.y+circle.r<this.y);
     }
     //koristim za proveru query-a
     draw(){
@@ -102,6 +140,18 @@ class QuadTree{
         this.southwest=new QuadTree(sw);
         let se=new Rectangle(this.bound.x+this.bound.width/2,this.bound.y+this.bound.height/2,this.bound.width/2,this.bound.height/2);
         this.southeast=new QuadTree(se);
+        for(let lopta of this.balls){
+            if(this.northwest.bound.contains(lopta)){
+                this.northwest.insert(lopta);
+            }else if(this.northeast.bound.contains(lopta)){
+                this.northeast.insert(lopta);
+            }else if(this.southwest.bound.contains(lopta)){
+                this.southwest.insert(lopta);
+            }else if(this.southeast.bound.contains(lopta)){
+                this.southeast.insert(lopta);
+            }
+        }
+        this.balls=[];
         this.divided=true;
     }
 
@@ -119,7 +169,7 @@ class QuadTree{
     //provereno je
     queryRange(range){
         let pointsInRange=[];
-        if(!this.bound.intersect(range)){
+        if(!this.bound.intersectCircle(range)){
             return pointsInRange;
         }
         for(let lopta of this.balls){
@@ -151,6 +201,16 @@ class Menager{
             loptica.draw(context);
         }
         quad.draw();
+        for(let lopta of this.lopte){
+            let range=new Circle(lopta.x,lopta.y,lopta.radius*2);
+            let lopteuRangeu=quad.queryRange(range);
+            for(let drugaLopta of lopteuRangeu){
+                if(lopta != drugaLopta && lopta.intersect(drugaLopta)){
+                    lopta.collision=true;
+                    drugaLopta.collision=false;
+                }
+            }
+        }
     }
 }
-
+//colision, dugme za menjanje radiusa
